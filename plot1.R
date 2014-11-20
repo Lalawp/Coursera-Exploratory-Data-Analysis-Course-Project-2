aa3ea3d79137c520b152443e3572e45b441017c0
Lalawp <jonston@gmail.com>
2014-11-20 10:50:30 -0800

    Plot 1 Added

---
+++ plot1.R
@@ -0,0 +1,31 @@
+##Function subsets National Emissions Inventory by year
+NEI99 <- NEI[which(NEI$year=="1999"), ]
+NEI02 <- NEI[which(NEI$year=="2002"), ]
+NEI05 <- NEI[which(NEI$year=="2005"), ]
+NEI08 <- NEI[which(NEI$year=="2008"), ]
+
+##Totals the emissions for each subset
+NEI99Mean <- as.numeric(sum(NEI99$Emissions))
+NEI02Mean <- as.numeric(sum(NEI02$Emissions))
+NEI05Mean <- as.numeric(sum(NEI05$Emissions))
+NEI08Mean <- as.numeric(sum(NEI08$Emissions))
+
+##Adds the year variable to each calculated mean
+NEI99Mean <- cbind(NEI99Mean, "1999")
+NEI02Mean <- cbind(NEI02Mean, "2002")
+NEI05Mean <- cbind(NEI05Mean, "2005")
+NEI08Mean <- cbind(NEI08Mean, "2008")
+
+##Rbinds the new dataframes together and adds a column name for the year
+NEIMean <- as.data.frame(rbind(NEI99Mean, NEI02Mean, NEI05Mean, NEI08Mean))
+colnames(NEIMean)[[1]]="PollutantMean"
+colnames(NEIMean)[[2]]="Year"
+
+NEIMean$PollutantMean <- as.numeric(as.character(NEIMean$PollutantMean))
+NEIMean$Year <- as.numeric(as.character(NEIMean$Year))
+
+plot(NEIMean$Year, NEIMean$PollutantMean, main="Total Emissions", type="l", xlab="Year", ylab="Total Pollutants by Tonne")
+
+dev.copy(png, "plot1.png")
+dev.off()
+
